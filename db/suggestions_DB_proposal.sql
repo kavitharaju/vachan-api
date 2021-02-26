87118b4d300ba5636120e37468ad84731c15599c
kavitharaju <kavitharaju18@gmail.com>
2021-02-26 15:39:25 +0530

    nlp module: revise table structures

--- db/suggestions_DB_proposal.sql
+++ db/suggestions_DB_proposal.sql
@@ -1,16 +1,5 @@
 -- Planning DB structure for AgMT suggestions Module
 
--- CREATE TABLE public.content_types (
---     content_type_id SERIAL PRIMARY KEY ,
---     content_type text UNIQUE NOT NULL
--- );
-
--- INSERT INTO content_types(content_type) VALUES('bible');
--- INSERT INTO content_types(content_type) VALUES('commentary');
--- INSERT INTO content_types(content_type) VALUES('dictionary');
--- INSERT INTO content_types(content_type) VALUES('infographics');
--- INSERT INTO content_types(content_type) VALUES('bible_video');
-
 CREATE TABLE public.languages (
     language_id SERIAL PRIMARY KEY,
     language_code char(3) UNIQUE NOT NULL,
@@ -20,101 +9,44 @@ CREATE TABLE public.languages (
 
 alter table languages alter column script_direction set default 'left-to-right';
 
--- \COPY languages (language_code,language_name) FROM 'languages.csv' DELIMITER ',' CSV HEADER;
-
--- CREATE TABLE public.versions (
---     version_id SERIAL PRIMARY KEY,
---     version_code text UNIQUE NOT NULL,
---     version_description text NOT NULL,
---     revision text NOT NULL,
---     metadata jsonb
--- );
-
--- CREATE TABLE public.sources (
---     source_id SERIAL PRIMARY KEY,
---     table_name text  UNIQUE NOT NULL,
---     year int NOT NULL,
---     license text DEFAULT 'CC BY SA',
---     content_id int NOT NULL
---       REFERENCES content_types(content_type_id) ON DELETE CASCADE,
---     language_id int NOT NULL
---       REFERENCES languages(language_id) ON DELETE CASCADE,
---     created_at_date timestamp with time zone DEFAULT ('now'::text)::timestamp(2) with time zone,
---     status boolean DEFAULT true NOT NULL,
---     version_id int
---       REFERENCES versions(version_id) ON DELETE CASCADE,
---     metadata jsonb 
--- );
-
-CREATE TABLE public.users(
-    user_id SERIAL PRIMARY KEY,
-    user_name text NOT NULL,
-    user_email text NOT NULL unique,
-    active boolean default true
-);
-
-CREATE TYPE sentence AS(
-    sentence_id int,
-    sentence_text text
-);
-
 CREATE TABLE public.translation_projects(
     project_id SERIAL PRIMARY KEY,
     project_name TEXT unique NOT NULL,
     source_lang_id int NOT NULL
-        REFERENCES languages(language_id) ON DELETE CASCADE,
+        REFERENCES languages(language_id),
     target_lang_id int NOT NULL 
-        REFERENCES languages(language_id) ON DELETE CASCADE,
+        REFERENCES languages(language_id),
     source_document_format text DEFAULT 'USFM',
     active boolean default true,
-    created_user int NULL
-        REFERENCES users(user_id) ON DELETE CASCADE,
+    created_user int NULL,
     created_at timestamp with time zone DEFAULT NOW()
+);
+
+CREATE TABLE public.translation_project_users(
+    project_id int REFERENCES translation_projects(project_id),
+    user_id int
 );
 
 CREATE TABLE public.translation_drafts(
     draft_id SERIAL PRIMARY KEY,
     project_id int NOT NULL 
         REFERENCES translation_projects(project_id) ON DELETE CASCADE,
-    sentence_id int NOT NULL unique,
+    sentence_id int NOT NULL,
     sentence text,
     draft text,
     draft_meta jsonb[],
-    -- created_at timestamp with time zone DEFAULT NOW(),
-    -- created_user int NULL,
-    last_updated_user int NULL
-        REFERENCES users(user_id) ON DELETE CASCADE,
-    last_updated_at  timestamp with time zone DEFAULT NOW()
+    last_updated_user int NULL,
+    last_updated_at  timestamp with time zone DEFAULT NOW(),
+    UNIQUE(project_id, sentence_id)
 );
-
-
-CREATE TABLE public.translation_project_users(
-    project_id int REFERENCES translation_projects(project_id),
-    user_id int REFERENCES users(user_id)
-);
-
--- CREATE TYPE occurance AS(
---     sentence_id int,
---     char_offset_start int,
---     char_offset_end int
--- );
-
--- CREATE TYPE suggestion AS(
---     suggestion text,
---     score numeric
--- );
 
 CREATE TABLE public.translation_memory(
-    translation_id int PRIMARY KEY,
-    project_id int REFERENCES translation_projects(project_id),
-    sentence_id int REFERENCES translation_drafts(sentence_id),
-    char_offset_start int NOT NULL,
-    char_offset_end int,
+    token_id int PRIMARY KEY,
+    source_lang_id int NOT NULL
+        REFERENCES languages(language_id),
+    target_lang_id int NOT NULL 
+        REFERENCES languages(language_id),
     token text NOT NULL,
-    -- verified_offset occurance NOT NULL,
-    verified_translation text,
-    last_updated_user int NULL
-        REFERENCES users(user_id) ON DELETE CASCADE,
-    last_updated_at  timestamp with time zone DEFAULT NOW()
+    translations text[],
+    UNIQUE(source_lang_id, target_lang_id, token)
 );
-
